3cbfc3c1e11329e25ccb156e5a067df6520398ce
Jakub <jakub.ratusz@gmail.com>
2024-04-11 15:28:23 +0200

    Initial commit

---
+++ NaiProject3/src/LanguageRecognizer.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LanguageRecognizer {
+    public static void main(String[] args) {
+        LanguageRecognizer lr = new LanguageRecognizer();
+        System.out.println(lr.loadLanguages());
+
+    }
+
+    private Map<String, List<String>> loadLanguages(){
+        Map<String, List<String>> languageMap = new HashMap<>();
+        try {
+            Files.walkFileTree(Paths.get("languages"), new SimpleFileVisitor<Path>() {
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+                    try {
+                        List<String> lines = Files.readAllLines(file);
+                        languageMap.put(file.getFileName().toString().split("\\.")[0], lines);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
+        return languageMap;
+    }
+}
